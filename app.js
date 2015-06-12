d2f933545c2f6f84b4e3c2a8aeb91bba0f7dcdf6
elf Pavlik <perpetual-tripper@wwelves.org>
2015-06-12 17:11:51 +0200

    minimal posting interface

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 $(function(){
-  axios.get('https://wwelves.org/perpetual-tripper/')
+  axios.get('https://wwelves.org/perpetual-tripper/log/', {
+    responseType: 'json'
+  })
   .then(function(response) {
     console.log(response.data);
   });
 
   console.log('READY!');
+
+  function enableEditor(){
+    $('#editor button').attr('disabled', null);
+    $('#editor textarea').attr('disabled', null);
+  }
+
+  function disableEditor(){
+    $('#editor button').attr('disabled', 'disabled');
+    $('#editor textarea').attr('disabled', 'disabled');
+  }
+
+  //debug
+  window.app = {};
+  window.app.enableEditor = enableEditor;
+  window.app.disableEditor = disableEditor;
 });
